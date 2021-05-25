5f3368f3f0e5fc40e7d6f8ddbffe51b5e0ca9e04
jefacb10 <32496187+jefacb10@users.noreply.github.com>
2021-05-25 12:13:47 -0300

    Adicionais

---
+++ Singleton/Servico.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Treinamento_DesignPattern_2.Singleton
+{
+    public class Servico
+    {
+        public string Nome { get; set; }
+    }
+}
---
+++ Singleton/ServicoSingleton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Treinamento_DesignPattern_2.Singleton
+{
+    public class ServicoSingleton
+    {
+        private static Servico _servico = new Servico();
+
+        public Servico Servico { get { return _servico; } }
+    }
+}
